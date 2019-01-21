f555e0afd592788368df8f3f2e9df27a12f773b0
Sanwood Gim <sanwoodgim@sanwoods-mbp.bc.edu>
2019-01-20 23:22:44 -0500

    Initial Commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Sanwood Gim on 1/20/19.
+//  Copyright © 2019 Sanwood Gim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
